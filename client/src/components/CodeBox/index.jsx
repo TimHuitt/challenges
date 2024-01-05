65413181028caac037c7cf5e289bcd2c95504a31
Tim Huitt <timhuitt@gmail.com>
2024-01-04 22:17:37 -0700

    add code editor

--- client/src/components/CodeBox/index.jsx
+++ client/src/components/CodeBox/index.jsx
@@ -1,10 +1,31 @@
 import './Codebox.css'
+import React from 'react'
+import 'prismjs'
+import Editor from 'react-simple-code-editor';
+import './prismjs/components/prism.css';
 
 const CodeBox = () => {
-  
+
+  const [code, setCode] = React.useState(
+    `console.log('Welcome!')
+  console.log('write some code here')`
+  );
+
   return (
     <div id="CodeBox">
-      <p>Code</p>
+      <Editor
+        value={code}
+        onValueChange={code => setCode(code)}
+        highlight={code => Prism.highlight(code, Prism.languages.js)}
+        padding={10}
+        style={{
+          height: '100%',
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 14,
+          border: '2px solid grey',
+          borderRadius: '10px'
+        }}
+      />
     </div>
   )
 }
