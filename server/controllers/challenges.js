e601882853bebecea86b7635e200babecfe629f8
Tim Huitt <timhuitt@gmail.com>
2024-01-08 01:03:39 -0700

    update prompt, update response parsing

--- server/controllers/challenges.js
+++ server/controllers/challenges.js
@@ -2,6 +2,7 @@ const OpenAI = require("openai");
 
 async function challenges(req, res) {
   const openai = new OpenAI();
+
   try {
     const completion = await openai.chat.completions.create({
       messages: [{ 
@@ -15,7 +16,9 @@ async function challenges(req, res) {
           - ID will be identifying information created and used by the AI to determine if the challenge has already been created
           - You may be presented with the same prompt multiple times, so the ID information MUST be specific to the challenge itself, including specific aspect of the challenge, such as the operation or task, in addition to including the parameters of the prompt ie: py_beg_short_count_vowels; ie: js_int_one-liner_is_even;
           - Always provide a unique challenge based on the history provided in the prompt. If a challenge is already present, you should create a new challenge
+          - Pay close attention to the 'difficulty' and tailor your challenge to match the requested skill level
           - If request details are provided, do your best to create a challenge adhering to this request
+          - Provide several test cases with the challenge. Doubly ensure the accuracy of expected outputs.
           - You will be given a prompt in the following structure:
             '''
             History: [a list of ID from all previously provided challenges]
@@ -31,10 +34,11 @@ async function challenges(req, res) {
             challenge: "details/instructions of the challenge to be presented to the user"
             textHints: ["Provide several hints to help the user figure out the solution. These hints should not provide any code specific snippets or references to command names or methods"]
             codeHints: ["Provide several hints that are code specific and help the user determine the exact code they should be using to solve this problem"]
-            testCases: ["Provided several test cases, including edge cases"]
+            testCases: ["input: test case input, output: "test case expected output"]
             Solution: "Provide the code for the optimal solution to your challenge."
             }
             '''
+          ${req.body}
         ` 
       }
     ],
