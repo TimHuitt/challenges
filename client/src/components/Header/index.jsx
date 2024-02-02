be28d3dedca624d73b6cf22bba54a35a476d386c
Tim Huitt <timhuitt@gmail.com>
2024-02-02 10:08:05 -0700

    merge updates, update header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -10,9 +10,9 @@ const Header = () => {
         <small>Where the AI tells <b><u>you</u></b> what to do</small>
       </div>
       <div id="header-right">
-        <div id="user-icon">
+        {/* <div id="user-icon">
           <p>User</p>
-        </div>
+        </div> */}
       </div>
     </div>
   )
